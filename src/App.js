fb06800f603ee6d4b6a967475e66277a33b7115f
David Son <son.david.b@gmail.com>
2021-04-25 16:17:50 -0400

    Added bio

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import {ART} from 'react-native'
-import avatar from './images/me.jpg'
+import avatar from './images/me.jpg';
 import './App.css';
 
 function spawnCircle() {
-  const circle = ART.Path()
-    .move(2, 0)
-    .arc(0, 4, 2)
-    .arc(0, -4, 2)
   
-  return circle.print()
 }
 
 function App() {
-  
-  
   return (
     <div className="App" onClick={spawnCircle}>
+      <h1 id="topText">
+        Hi there!
+      </h1>
       
       <header className="App-header">
-        <p>
-          Hi there!
-        </p>
-        <img id="avatar" src={avatar} alt="Me"></img>
+        <img src={avatar} alt="Me" id="avatar"></img>
+        <h2 class="heading">About me</h2>
+        <p id="bio">My name is David Son, and I'm currently a 
+          sophomore working towards a bachelor's at the 
+          Stevens Institute of Technology. I've been coding since 
+          I was 10 years and haven't stopped since. With 6 years of education 
+          and counting and 2 years of industry experience, I 
+          look forward to continuing to code for as long as I can!</p>
       </header>
+
+      <div classname="Education">
+        
+      </div>
+      
     </div>
   );
 }
